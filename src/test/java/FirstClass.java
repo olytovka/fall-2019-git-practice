4b8d14fd5cdeb278a3b8b4916e81e9914be63c4a
Olha <58573698+olytovka@users.noreply.github.com>
2020-03-25 19:54:34 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World :)");
-        System.out.println("Change one");
+        System.out.println("Change one"); //change from remote
         System.out.println("Change two");
         System.out.println("Change three");
         //first change
