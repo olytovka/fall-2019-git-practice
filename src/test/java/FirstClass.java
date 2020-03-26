9e0112bb0d1f4db89f608f5d8c6e3d5f4f29c489
Olha Lytovka <olha@Olhas-Air.fios-router.home>
2020-03-25 20:27:49 -0400

    new change

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World :)");
         System.out.println("Change one"); //change from remote
+        System.out.println("Hello World :)");//comment change from local
+        System.out.println("Change one");
         System.out.println("Change two");
         System.out.println("Change three");
         //first change
