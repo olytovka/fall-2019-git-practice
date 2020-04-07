8caf9cfb074b239d245826aa8303af3b60fc1733
olytovka <olhalytovka@gmail.com>
2020-04-07 18:06:30 -0400

    added another line

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,5 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("change from local one");
+     //   System.out.println("hi everyone!");
     }
 }
